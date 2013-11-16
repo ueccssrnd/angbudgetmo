c7a3ba9e57e3533351408facefe7aa43f99fe6dc
Joey Hipolito <hi@joeyhipolito.com>
2013-11-17 00:17:59 +0800

    Add content lightbox

--- assets/js/core.coffee
+++ assets/js/core.coffee
@@ -9,6 +9,7 @@ class BudgetMo
   init: () ->
     this.ui.build()
     this.listen()
+    Visuals::tempLightboxGraph()
   listen: () ->
     $('#lightbox').on 'click', '.light-close', (e) ->
       BudgetMo::ui.lightbox.hide()
@@ -23,12 +24,9 @@ class BudgetMo
 
     lightbox: {
       show: () ->
-        $('.overlay').show()
         $('#lightbox').show()
       hide: () ->
-        $('.overlay').hide()
         $('#lightbox').hide()
-
     }
   }
 
@@ -42,14 +40,79 @@ class Visuals
     }
   }
   init: () ->
-    height = 500
-    width = 500
+    height = 400
+    width = 400
     color = d3.scale.category20b()
     radius = 150
+  tempLightboxGraph: () ->
+    height = '100%'
+    width = '100%'
+    sum = (arr) ->
+      sum = 0
+      for i in arr
+        sum += i
+      return sum
+    otherValues = [30, 10, 80]# total of other values
+    sumOtherVal = sum(otherValues)
+    data = [50, sumOtherVal]
 
-  
+    color = ["#fcc427", "#888"]
 
+    radius = 150
+    
+    canvas = d3.select(".graph-preview .graph")
+              .append("svg")
+              .style("width", width)
+              .style("height", height)
+    arc = d3.svg.arc()
+          .outerRadius(radius)
+          .innerRadius(radius * 0.8)
+    layout = d3.layout.pie().sort(null)
+            .value( (d) ->
+              return d
+            )
+    group = canvas.append("g")
+            .attr("transform", "translate(200,200)")
 
+    group.append("circle")
+          .attr("cx", 0)
+          .attr("cy", 0)
+          .attr("r", radius * 0.8)
+          .attr("fill", "white")
+    group.append("image")
+            .attr("xlink:href", "assets/img/social-150.png")
+            .attr("x", (radius) / 2 * -1)
+            .attr("y", (radius) / 2 * -1)
+            .attr("width", radius)
+            .attr("height", radius)
+
+    arcs = group.selectAll(".arc")
+            .data(layout(data))
+            .enter()
+            .append("g")
+            .attr("class", "arc")
+    doughnut = arcs.append("path")
+              .attr("d", arc)
+              .attr("fill", (d, i) ->
+                return color[i]
+              )
+              .attr("stroke", "#fff")
+              .attr("stroke-width", 2)
+    $("#slider").slider({
+      value: data[0]
+      min: 0
+      max: sumOtherVal + data[0]
+      step: 1
+      slide: (e, ui) ->
+        update(data[0], ui.value)
+    })
+
+    update = (old,value) ->
+      newData = [value, sumOtherVal + old - value]
+      difference = Math.floor(Math.round((sumOtherVal - newData[1]) / otherValues.length))
+      console.log difference
+      arcs.data(layout(newData));
+      arcs.select("path").attr("d", arc)
 $(document).ready () ->
   app = new BudgetMo
   app.init()
--- assets/js/core.js
+++ assets/js/core.js
@@ -13,7 +13,8 @@ BudgetMo = (function() {
 
   BudgetMo.prototype.init = function() {
     this.ui.build();
-    return this.listen();
+    this.listen();
+    return Visuals.prototype.tempLightboxGraph();
   };
 
   BudgetMo.prototype.listen = function() {
@@ -31,11 +32,9 @@ BudgetMo = (function() {
     build: function() {},
     lightbox: {
       show: function() {
-        $('.overlay').show();
         return $('#lightbox').show();
       },
       hide: function() {
-        $('.overlay').hide();
         return $('#lightbox').hide();
       }
     }
@@ -58,10 +57,59 @@ Visuals = (function() {
 
   Visuals.prototype.init = function() {
     var color, height, radius, width;
-    height = 500;
-    width = 500;
+    height = 400;
+    width = 400;
     color = d3.scale.category20b();
     return radius = 150;
+  };
+
+  Visuals.prototype.tempLightboxGraph = function() {
+    var arc, arcs, canvas, color, data, doughnut, group, height, layout, otherValues, radius, sum, sumOtherVal, update, width;
+    height = '100%';
+    width = '100%';
+    sum = function(arr) {
+      var i, _i, _len;
+      sum = 0;
+      for (_i = 0, _len = arr.length; _i < _len; _i++) {
+        i = arr[_i];
+        sum += i;
+      }
+      return sum;
+    };
+    otherValues = [30, 10, 80];
+    sumOtherVal = sum(otherValues);
+    data = [50, sumOtherVal];
+    color = ["#fcc427", "#888"];
+    radius = 150;
+    canvas = d3.select(".graph-preview .graph").append("svg").style("width", width).style("height", height);
+    arc = d3.svg.arc().outerRadius(radius).innerRadius(radius * 0.8);
+    layout = d3.layout.pie().sort(null).value(function(d) {
+      return d;
+    });
+    group = canvas.append("g").attr("transform", "translate(200,200)");
+    group.append("circle").attr("cx", 0).attr("cy", 0).attr("r", radius * 0.8).attr("fill", "white");
+    group.append("image").attr("xlink:href", "assets/img/social-150.png").attr("x", radius / 2 * -1).attr("y", radius / 2 * -1).attr("width", radius).attr("height", radius);
+    arcs = group.selectAll(".arc").data(layout(data)).enter().append("g").attr("class", "arc");
+    doughnut = arcs.append("path").attr("d", arc).attr("fill", function(d, i) {
+      return color[i];
+    }).attr("stroke", "#fff").attr("stroke-width", 2);
+    $("#slider").slider({
+      value: data[0],
+      min: 0,
+      max: sumOtherVal + data[0],
+      step: 1,
+      slide: function(e, ui) {
+        return update(data[0], ui.value);
+      }
+    });
+    return update = function(old, value) {
+      var difference, newData;
+      newData = [value, sumOtherVal + old - value];
+      difference = Math.floor(Math.round((sumOtherVal - newData[1]) / otherValues.length));
+      console.log(difference);
+      arcs.data(layout(newData));
+      return arcs.select("path").attr("d", arc);
+    };
   };
 
   return Visuals;
